fc4168776c0d6cde92c378b069f9ab285beba701
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-26 15:14:36 -0300

    .

---
+++ aula11/script.js
@@ -0,0 +1,9 @@
+const num1 = prompt('Digite um número');
+const num2 = prompt('Digite outro número');
+let op1 = Number(num1)
+let op2 = Number(num2)
+let res = op1+ op2
+let resultado = document.getElementById('resultado')
+
+
+alert(`A soma dos dois números é ${res}`);
