9d118dbb506cd1935963e161b8fe19f4091c6074
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-26 16:41:31 -0300

    .

---
+++ aula12/index.js
@@ -0,0 +1,17 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+
+//array
+[varA, varB, varC] = [varB, varC, varA]
+
+/*const varATemp = varA;
+varA = varB;
+varB = varC;
+varC = varATemp;*/
+
+console.log(varA, varB, varC);
+
+//ou maneira morderna
+
