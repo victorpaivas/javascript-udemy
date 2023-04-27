ce031c0443a2ececc38a38d88e09e479ac34a02b
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-27 14:50:18 -0300

    .

---
+++ aula13/index.js
@@ -0,0 +1,6 @@
+// string são indexadas
+//               012345678
+let umaString = 'O rato roeu a roupa do rei de Roma.';
+
+console.log(umaString.toUpperCase())
+console.log(umaString.toLowerCase())
