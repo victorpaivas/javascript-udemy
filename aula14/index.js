39aa9462af6f26eb8086fc4c436c82d2ccfc544d
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-05-03 15:39:05 -0300

    .

---
+++ aula14/index.js
@@ -0,0 +1,17 @@
+//IEEE 754-2008
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2; 
+num1 += num2; 
+num1 += num2; 
+
+console.log(num1);
+
+// num1 = num1.toString(); alterar o tipo da váriavel num1 de number para string
+//console.log(num1.toString() + num2);
+//console.log(Number.isInteger(num2));  --> se o número é inteiro
+//let temp = num1 * 'Olá';
+//console.log(Number.isNaN(temp))
+//console.log(temp)
---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+console.log(100 / 0); // retornar infinity
+
+//console.log(num1 ** 0.5); -> raiz quadrada
+
+
+//let num2 = Math.floor(num1); -> arredondando para baixo
+//let num2 = Math.ceil(num1);  -> arredondando para cima
+//let num2 = Math.round(num1) -> arredondada automático para o número mais próximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500, 9,8,7,6)); -> maior número na sequencia
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500, 9,8,7,6)); -> menor número na sequencia
+//const aleatorio = Math.round(Math.random() * (10 -5) + 5); -> números aleatórios arredondados pelo round que ocorrem entre 5 e 10
