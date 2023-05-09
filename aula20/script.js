cfd4d446277f766cc4139323ecb50d27e5f9018d
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-05-09 14:39:15 -0300

    .

--- aula20/script.js
+++ aula20/script.js
@@ -2,6 +2,8 @@ function meuEscopo () {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
+    
+
     const pessoas = [];
 
   function recebeEventoForm (evento) {
@@ -12,9 +14,17 @@ function meuEscopo () {
      const peso = form.querySelector('.peso');
      const altura = form.querySelector('.altura');
     
-     const obj = {nome, sobrenome, peso, altura};
+     const obj = {
+      nome: nome.value, 
+      sobrenome: sobrenome.value, 
+      peso: peso.value, 
+      altura: altura.value
+    };
+
      pessoas.push(obj);
      console.log(pessoas);
+
+     resultado.innerHTML += `<p><strong>Nome:</strong> ${nome.value}<br> <strong>Sobrenome:</strong> ${sobrenome.value} <br><strong>Peso:</strong> ${peso.value}<br> <strong>Altura:</strong> ${altura.value}<br></p>`;
   }   
     form.addEventListener('submit', recebeEventoForm);
 }
