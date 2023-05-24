216107334af076afa8ea48968e0de037cd097a8b
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-05-24 13:30:47 -0300

    .

---
+++ exerciciolog/index2.js
@@ -0,0 +1,8 @@
+//Escreva uma função chamada ePaisegem que recebe dois argumentos, largura e altura de uma iamgem (number).
+//Retorne true se a imagem estiver no modo paisagem.
+
+let a = 1250;
+let c = 1950;
+const ePaisagem = () => a > c ? false : true;
+
+console.log(ePaisagem());
