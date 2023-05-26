5dc27ec8fc4ae3c0bee5ce5b9c9e1c57194cb4d7
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-05-26 14:50:39 -0300

    .

--- aula36/index.js
+++ aula36/index.js
@@ -1,13 +1,15 @@
 function mostraHora() {
     let data = new Date();
-
+    // função para mostrar hora
     return data.toLocaleTimeString('pt-BR', {
         hour12: false
     });
 }
-
-
-
-setInterval(function (){
-    
-}, 1000);
+// mostra a hora de tempo em tempo de acordo com o parâmetro que eu colocar
+const timer = setInterval(function (){
+    console.log(mostraHora());
+}, 1000); // primeiro vem a função depois o parâmetro do intervalo em milesimo de segundos.
+//função para parar o setInterval
+setTimeout(function () {
+    clearInterval(timer);
+}, 3000);  // primeiro vem a função depois o parâmetro da parada em milesimo de segundos igual ao setinterval.
