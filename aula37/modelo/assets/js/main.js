79cb67b58580fed5a366e32a5bb7b307deba21fc
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-05-31 14:45:23 -0300

    .

---
+++ aula37/modelo/assets/js/main.js
@@ -0,0 +1,80 @@
+const inputTarefa = document.querySelector('.input-tarefa')
+const btnTarefa = document.querySelector('.btn-tarefa')
+const tarefas = document.querySelector('.tarefas')
+
+function criarLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaTarefa(textoInput) {
+    const li = criarLi();
+    li.innerHTML = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criarBotaoApagar(li);
+    salvarTarefas();
+}
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+        
+    }
+    // transforma as tarefas em json em string para salvar e posteriormente abrir com minhas tarefas criadas salva.
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+
+    // transforma de JSON para JavaScript novamente para poder exibir minhas tarefas.
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+   
+    for (let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
+
+function criarBotaoApagar(li) {
+    const botaoApagar = document.createElement('button');
+    li.innerText += ' ';
+    botaoApagar.innerText = 'Apagar'
+    //  botaoApagar.classList.add('apagar');
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+
+btnTarefa.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove()
+        salvarTarefas();
+    }
+
+});
