b5130942cf7ab0e2a3b98b020f4752f4389137da
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-06-05 14:51:50 -0300

    .

---
+++ aula38/index.js
@@ -0,0 +1,40 @@
+// Funções são First-class objects (objetos de primeira classe)
+// declaração de função (function hoisting)
+
+function falaOi() {
+    console.log('Oi!')
+}
+
+falaOi();
+
+// Function expression
+const souUmDado = function (){
+    console.log('Sou um dado.');
+};
+
+
+function executaFuncao(funcao){
+    console.log('Vou executar sua função abaixo');
+    funcao();
+}
+souUmDado();
+
+// Arrow Function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto posso ter uma function
+
+const obj = {
+    falar1: function(){
+        console.log('Estou falando...')
+    }, // posso declarar direto a function e vai se tornar um método dentro do objeto.
+    falar2() {
+        console.log('Estou falando 2')
+    }
+};
+
+obj.falar1();
+obj.falar2();
---
+++ aula38/index2.js
@@ -0,0 +1,5 @@
+// A função declarada com a palavra inicial function ou as function express tem uma váriavel especial que sustena todos os argumentos enviados chamada arguments.
+function funcao(){
+    console.log(arguments)
+}
+funcao('Valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
