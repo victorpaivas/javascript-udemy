4967d7e1e248d60a2786a70b5e573580d154a155
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-06-12 13:17:31 -0300

    .

--- aula38/index2.js
+++ aula38/index2.js
@@ -1,5 +1,25 @@
 // A função declarada com a palavra inicial function ou as function express tem uma váriavel especial que sustena todos os argumentos enviados chamada arguments.
-function funcao(){
-    console.log(arguments)
+// preciso mandar o valor ´undefined´ para um argumento ir vazio.
+const conta = function conta(operador, acumulador,...numeros) {
+    for(let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '/') acumulador /= numero;
+        if (operador === '*') acumulador *= numero;
+    }
+    console.log(acumulador);
+};
+conta('+', 5, 20, 30, 40, 50);
+
+
+function funcao1({nome, sobrenome, idade}) {
+    console.log(nome, sobrenome, idade);
 }
-funcao('Valor', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+let obj =  {nome: 'Victor', sobrenome: 'Silva', idade: 27 }
+funcao1(obj)
+
+function funcao({a, b = 2,c = 4}){
+    //b = b || 0;  forma antiga 
+    console.log(a + b + c);
+}
+funcao(2, undefined, 20);
