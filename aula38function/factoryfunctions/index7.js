ba2ab60e1416afe00506420281f62dde359d1842
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-06-21 17:37:03 -0300

    Update index7.js

--- aula38function/factoryfunctions/index7.js
+++ aula38function/factoryfunctions/index7.js
@@ -1,12 +1,39 @@
-function criaPessoa(nome, sobrenome) {
+// factory function (função fábrica)
+function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome,
         sobrenome,
-        fala: function(assunto){
+
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome =
+            console.log(valor)
+        },
+
+        fala: function (assunto = 'falando sobre a vida') {
             return `${this.nome} está ${assunto}.`
+        },
+        altura,
+        peso,
+        // Getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2);
         }
-    }
+    };
 }
 
-const p1 = criaPessoa('Victor', 'Silva');
-console.log(p1);
+const p1 = criaPessoa('Victor', 'Paiva', 1.75, 113);
+const p2 = criaPessoa('José', 'Silva', 1.70, 78);
+const p3 = criaPessoa('Maria', 'Rodrigues', 1.65, 58);
+
+
+
+
