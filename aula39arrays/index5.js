886bb7c18ceadc3bbcc48cd67c6cfdd07871d1be
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-07-03 14:15:33 -0300

    ..

---
+++ aula39arrays/index5.js
@@ -0,0 +1,24 @@
+// Reduce
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+// Retorne um array com o dobro dos valores (Map)
+// const total = numeros.reduce(function(acumulador, valor){
+//     acumulador.push(valor * 2)
+//  return acumulador;
+// }, []);
+// console.log(total);
+
+// Some todos os números (reduce)
+// const total = numeros.reduce(function(acumulador, valor, indice, array){
+//     acumulador += valor;
+//  return acumulador;
+// }, []);
+// console.log(total);
+
+// Retorne um array com pares (Filter)
+// const total = numeros.reduce(function(acumulador, valor, indice, array){
+//     if(valor % 2 ===0) acumulador.push(valor);
+//  return acumulador;
+// }, []);
+// console.log(total);
