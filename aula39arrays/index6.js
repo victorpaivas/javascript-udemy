575c1d8a438dfa6cf1e55cc4b9686a18854fa5de
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-07-04 15:17:58 -0300

    .

--- aula39arrays/index6.js
+++ aula39arrays/index6.js
@@ -5,24 +5,11 @@
 
 // Retorna
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const numerosPares = numeros.filter(function(valor) {
-    if (valor % 2 === 0) {
-        return valor;
-    }
-});
-
-console.log(numerosPares);
-
-const dobraValores = numeros.map(function(valor) {
-    return valor * 2;
-})
-
-// console.log(dobraValores);
-
-
-const somarTudo = numeros.reduce(function(acumulador, valor) {
+const numerosPares = numeros.filter(valor => valor % 2 === 0).map(valor => valor * 2).reduce(function(acumulador, valor) {
     acumulador += valor;
     return acumulador;
 }, 0);
 
-// console.log(somarTudo);
+
+console.log(numerosPares);
+
