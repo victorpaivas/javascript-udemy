e0c519c978921af6750d6121e70772bbf06f6821
victorpaivas <129319485+victorpaivas@users.noreply.github.com>
2023-08-01 14:06:36 -0300

    .

--- exerciciocpf/index.js
+++ exerciciocpf/index.js
@@ -1,6 +1,6 @@
 // Validador de CPF
 
-//  546.522.618-88
+//  546.522.618-88  ...   070.987.720-03
 
 /*
 CONTA PARA VALIDAR O CPF
@@ -23,6 +23,7 @@ se o digito for maior que 9 consideramos 0
 // let cpfLimpo = cpf.replace(/\D+/g, '');
 // cpfArray = Array.from(cpfLimpo);
 
+// Função Construtora retornando com replace para substituir pontos e traços por espaços vazios.
 function ValidaCPF(cpfEnviado) {
     Object.defineProperty(this, 'cpfLimpo', {
         get: function () {
@@ -32,14 +33,21 @@ function ValidaCPF(cpfEnviado) {
 
 }
 
+// Área que identifica se o CPF é válido ou não 
 ValidaCPF.prototype.valida = function () {
+    // se o campo cpf tiver vázio retornará cpf inválido
     if (typeof this.cpfLimpo === 'underfined') return false;
+    // se o cpf tiver menos que 11 carácteries retornará cpf inválido
     if (this.cpfLimpo.length !== 11) return false;
+    // se o cpf tiver sequência de números iguais retornará cpf inválido 
+    if(this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
 
-    return true;
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
 };
 
 
@@ -47,12 +55,26 @@ ValidaCPF.prototype.criaDigito = function (cpfParcial) {
     const cpfArray = Array.from(cpfParcial)
 
     let regressivo = cpfArray.length + 1;
-    const digito = cpfArray.reduce((ac, val) => {
-        console.log(regressivo, val, regressivo * val)
+    let total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
         regressivo--;
         return ac;
     }, 0)
+    
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
 };
+ // Código para não permitir sequência no CPF exemplo 111.111.111-11
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo;
+}
 
 const cpf = new ValidaCPF('546.522.618-88');
-console.log(cpf.valida());
+
+// Área que exibe para o usuário se o CPF é válido ou não.
+if(cpf.valida()) {
+    console.log('CPF Válido')
+} else {
+    console.log('CPF inválido')
+}
