e457aa813e191446a5ecc3f09a18644b9d19f4d8
victorpaivas <129319485+victorpaivas@users.noreply.github.com>
2023-08-09 16:36:51 -0300

    Create index3.js

---
+++ aula41poo/index3.js
@@ -0,0 +1,39 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if(this.ligado) {
+            console.log(`${this.nome} já ligado`);
+            return;
+        }
+
+        this.ligado = true;
+    }
+
+    desligar() {
+        if(this.ligado) {
+            console.log(`${this.nome} já desligado`);
+            return;
+        }
+
+        this.ligado = false;
+    }
+}
+
+class Smartphone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome); 
+
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy s10')
+
+console.log(s1);
