4c958877eda09f8b5c76081cec2acda91d065107
Victor Paiva <victorrdps7@gmail.com>
2023-08-25 15:40:24 -0300

    Commit curso Udemy

--- aula42assinc/index3.js
+++ aula42assinc/index3.js
@@ -1,6 +1,6 @@
 // Async / Await
 
-function rand(min, max) {
+function rand(min=0, max=3) {
     max *= 1000;
     min *= 1000;
     return Math.floor(Math.random() * (max - min) + min);
@@ -16,5 +16,36 @@ function esperaAi(msg, tempo) {
     });
 }
 
-esperaAi('Fase 1', rand(0, 3))
-    .then(valor => console.log(valor))
+// esperaAi('Fase 1', rand())
+//     .then(valor => {
+//         console.log(valor);
+//         return esperaAi('Fase 2', rand())
+//     })
+//     .then(fase => {
+//         console.log(fase);
+//         return esperaAi('Fase 3', rand())
+//     })
+//     .then(fase => {
+//         console.log(fase)
+//         return fase;
+//     })
+//     .then(fase => {
+//         console.log('Terminamos na fase', fase)
+//     })
+//     .catch(error => console.log(error))
+
+
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase 1', rand());
+        console.log(fase1);
+        const fase2 = await esperaAi('Fase 2', rand());
+        console.log(fase2);
+        const fase3 = await esperaAi('Fase 3', rand());
+        console.log(fase3);
+    } catch(e){
+        console.log(e);
+    }
+
+}
+executa()
